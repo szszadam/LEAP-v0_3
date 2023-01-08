f3d9764021c1939a0e00e3bcd6edee8325cda698
szszadam <szszadam@gmail.com>
2023-01-08 21:54:02 +0100

    Revert "Sixth and seventh comment section are added"
    This reverts commit 7ae43b1e020c7a9e6840a53ca67fda9c557e7e0a.

--- LEAP-v0_3/Model-Classes/Administrator.cs
+++ LEAP-v0_3/Model-Classes/Administrator.cs
@@ -9,7 +9,6 @@ namespace LEAP_v0_3
     class Administrator : Teacher
     {
         /*fifth comment section*/
-        /*sixth comment section*/
         public Administrator
             (int _sql_id, string _familyName, string _firstName, string _userIdentificationNumber, string _passwordHashCode, DateTime _registrationTime, string _authorizationLevel, string _subjectsTaught) : base ( _sql_id,  _familyName,  _firstName,  _userIdentificationNumber,  _passwordHashCode,  _registrationTime,  _authorizationLevel,  _subjectsTaught)
         {
