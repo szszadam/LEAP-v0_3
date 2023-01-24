2279fedb30a412d015e89692e7b6a067dfe7c60f
szszadam <szszadam@gmail.com>
2023-01-24 23:35:36 +0100

    Description of Administrator class

--- LEAP-v0_3/Model-Classes/Administrator.cs
+++ LEAP-v0_3/Model-Classes/Administrator.cs
@@ -8,8 +8,20 @@ namespace LEAP_v0_3
 {
     class Administrator : Teacher
     {
-        /*fifth comment section*/
-        /*sixth comment section*/
+        //
+        // Administrator class
+        //
+        // Class description:
+        //
+        // Administrator level users appear as objects of this type when
+        // the program is running.It is a descendant of the Teacher class and,
+        // with it, of the User class.
+        //
+        // Fields:
+        // (beyond the fields of the "User" class)
+        //
+        // _ subjectsTaughtList: List<string> - it is used to store the subjects taught by the administrator.
+
         public Administrator
             (int _sql_id, string _familyName, string _firstName, string _userIdentificationNumber, string _passwordHashCode, DateTime _registrationTime, string _authorizationLevel, string _subjectsTaught) : base ( _sql_id,  _familyName,  _firstName,  _userIdentificationNumber,  _passwordHashCode,  _registrationTime,  _authorizationLevel,  _subjectsTaught)
         {
