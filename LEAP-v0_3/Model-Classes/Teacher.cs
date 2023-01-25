48d3b57739fa66c42c8c1c3535cbd649bb63a6f6
szszadam <szszadam@gmail.com>
2023-01-25 19:46:00 +0100

    Description of the Teacher class

--- LEAP-v0_3/Model-Classes/Teacher.cs
+++ LEAP-v0_3/Model-Classes/Teacher.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace LEAP_v0_3
 {
-    /*fourth comment section*/
+    //      *****Teacher class*****
+    //
+    // Users with the teacher authorization level appear as objects of this type when
+    // the program is running.Teacher class is descendant of the “User” class.
+    //
+    //      ***Fields***
+    //      (beyond the fields of the "User" class)
+    //
+    // _subjectsTaughtList: List<string> - it is used to store the subjects taught by the teacher.
+
     class Teacher : User
     {
         List<string> _subjectsTaughtList = new List<string>();
