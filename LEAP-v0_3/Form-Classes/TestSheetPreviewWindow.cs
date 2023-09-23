c1471d580196ce880676b6b88b7fb41910baa085
szszadam <szszadam@gmail.com>
2023-09-23 07:23:31 +0200

    Correction of description of the Test Sheet Preview Window class

--- LEAP-v0_3/Form-Classes/TestSheetPreviewWindow.cs
+++ LEAP-v0_3/Form-Classes/TestSheetPreviewWindow.cs
@@ -15,7 +15,9 @@ namespace LEAP_v0_3
 {
     //      *****Test Sheet Preview Window class*****
     //
+    //
     //      ***Class description***
+    //
     //
     // A class for opening the preview of the test sheet created in the test sheet editor,
     // which is a descendant of the "Form" class. The collected multiple-choice and essay
@@ -24,16 +26,21 @@ namespace LEAP_v0_3
     // options for the multiple-choice questions, the correct answers will be colored green here,
     // and here there is no countdown timer.
     //
+    //
     //      ***Fields***
+    //
     //
     // CurrentEditedTestSheet:EditedTestSheet – Stores a reference to the "Edited Test Sheet"
     // object on which the preview window is based.
     //
-    //      ***Methods and events***
+    //
+    //      ***Methods and event handlers***
+    //
     //
     // TestSheetWindow_Load() event - An event that raises when the test sheet preview window
     // is opened, as a result of it, the FillTestSheetFlowLayoutPanel() method is called.
     // The subject and the topic properties of the test sheet will be showed on the data label.
+    //
     //
     // FillTestSheetFlowLayoutPanel() – the method populates the "FlowLayoutPanel" control
     // on the graphic interface with the elements of the task list of the edited test sheet.
