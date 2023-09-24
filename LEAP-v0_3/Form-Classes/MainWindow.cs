971e5807e1492c8e9ed6be56eb3318b8da1be1ff
szszadam <szszadam@gmail.com>
2023-09-24 17:16:12 +0200

    Description of the Main Window Class

--- LEAP-v0_3/Form-Classes/MainWindow.cs
+++ LEAP-v0_3/Form-Classes/MainWindow.cs
@@ -1,18 +1,110 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
 namespace LEAP_v0_3
 {
     public partial class MainWindow : Form
     {
+        
+    //      ***** Main Window Class *****
+    //
+    //
+    //      *** Class description ***
+    //
+    //
+    // The main window class is a descendant of the "Form" class and provides navigation between the menu items
+    // that implement the main functions, as well as displays on its panel interface the access point of the
+    // main functions that are descendants of the "UserControl" class. The main elements of the graphic
+    // interface are : "Solve test sheet" button (A, T, S), "View results" button(A, T, S),
+    // "Edit tasks" button(A, T), "Edit test sheet" button(A , T), "Send test sheets" button(A, T),
+    // "Check test sheets" button(A, T), "Manage users" button(A), "Sign out" button(A, T, S)
+    // and the "Change password" button(A, T, S).
+    //
+    // The letters behind the button names(between the parentheses) indicate the user's required authorization
+    // level for accessing the function, therefore the visibility of the button representing the function.
+    // ('A'-Administrator, 'T'-Teacher, 'S'-Student). Furthermore, part of this graphical interface is a panel
+    // control, in which the following "UserControl" controls are embedded:
+    // "TestSheetSelectorUC", "TestSheetCheckerSelectorUC", "TestSheetViewerSelectorUC",
+    // "TaskSelectorAndEditorUC", "TestSheetAssemblerSelectorUC", "UserManagementSelectorUC”
+    //
+    //
+    //      *** Methods and event handlers ***
+    //
+    //
+    // MainWindow_Load() event handler - this method runs when the main window is opened, which causes the main
+    // data of the logged-in user to be written into the bar of the bottom of window.
+    //
+    //
+    // PanelSwitch() – this method switches between the "UserControl" controls embedded in the main window.It
+    // makes visible and moves foregrounds the control it receives as a parameter, and hides the rest.
+    //
+    //
+    // SolveTestSheetButton_Click() event handler – this method runs when the “Solve test sheet” button is
+    // clicked and as a result, the PanelSwitch() method makes one instance of the "TestSheetSelectorUC" class
+    // embedded in the main window, and activates and prioritizes it(bring it to the front).
+    //
+    //
+    // CheckTestSheetsButton_Click() event handler - this method runs when the “Check test sheet” button is
+    // clicked and as a result, the PanelSwitch() method makes one instance of the "TestSheetCheckerSelectorUC"
+    // class embedded in the main window, then activates and prioritizes it(bring it to the front).
+    //
+    //
+    // ViewResultsButton_Click() event handler -this method runs when the “View results” button is clicked and
+    // as a result, the PanelSwitch() method makes one instance of the "TestSheetViewerSelectorUC" class
+    // embedded in the main window, then activates and prioritizes it(bring it to the front).
+    //
+    //
+    // EditTasksButton_Click() event handler - this method runs when the “Edit tasks” button is clicked and as a
+    // result, the PanelSwitch() method makes one instance of the "TaskSelectorAndEditorUC" class embedded in
+    // the main window, then activates and prioritizes it(bring it to the front).
+    //
+    //
+    // EditTestSheetsButton_Click() event handler - this method runs when the “Edit test sheet” button is
+    // clicked and as a result, the PanelSwitch() method makes one instance of the
+    // "TestSheetAssemblerSelectorUC" class embedded in the main window, then activates and prioritizes it
+    // (bring it to the front).
+    //
+    //
+    // ManageUsersButton_Click() event handler -  this method runs when the “Manage users” button is clicked and
+    // as a result, the PanelSwitch() method activates an instance of the "UserManagementSelectorUC" class
+    // embedded in the main window, then activates and prioritizes it(bring it to the front).
+    //
+    //
+    // SendTestSheetsButton_Click() event handler - this method runs when the “Send test sheet” button is
+    // clicked and as a result, it creates a new instance of the "IndividualTestSheetSendingWindow" class and
+    // opens it in a new dialog window.
+    //
+    //
+    // SignOutButton_Click() event handler – by clicking on the "Logout" button, the current main window will be
+    // closed and a new instance of the "SingInWindow" class and opens it in a new dialog window.
+    //
+    //
+    // ChangePasswordButton_Click() event handler -  clicking on the "Change Password" button creates a new
+    // instance of the "PasswordChangeWindow" class and opens it in a new dialog window.
+    //
+    //
+    // MainWindow_Activated() event handler –  this method runs when the main window becomes active again.
+    // Typically, this occurs when a dialog window implementing one of the main functions is closed and the main
+    // window becomes active again. As a result, the "DataGridView" data row display control embedded in the
+    // main window, located on all "UserControl" controls, updates its content (reads the data again from the
+    // static lists of the static "DB_Connection" class).
+
+
+
         public MainWindow()
         {
             InitializeComponent();
