e24316cbb45a4a770906c8466e63f22502ad4961
szszadam <szszadam@gmail.com>
2023-09-24 10:52:05 +0200

    Description of the Multiple Choice Task User Control Class

--- LEAP-v0_3/Form-Classes/MultipleChoiceTaskUC.cs
+++ LEAP-v0_3/Form-Classes/MultipleChoiceTaskUC.cs
@@ -8,11 +8,51 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
 
 namespace LEAP_v0_3
 {
     public partial class MultipleChoiceTaskUC : UserControl
     {
+
+    //      ***** Multiple Choice Task User Control Class *****
+    //
+    //
+    //      *** Class description ***
+    //
+    //
+    // This class is a descendant of the "UserControl" class and it provides the display of multiple-
+    // choice tasks on the testsheet solving interface.
+    //
+    //
+    //      *** Fields ***
+    //
+    //
+    // _question: string –   is used to store the wording of the question / task
+    //
+    //
+    // _answerOptions: List<string> - collection of response options
+    //
+    //
+    //      *** Methods and event handlers ***
+    //
+    //
+    // MultipleChoiceTaskUC_Load() event handler - when the Multiple Choice Task User Control is
+    // loaded, it calls the LoadAnswerOptions() method.
+    //
+    //
+    // LoadAnswerOptions() – it places the " MultipleChoiceAnswerOptionUC" instances in the
+    // "FlowLayoutPanel" placed on the Multiple Choice Task User Control.
+    //
+    //
+    // MultipleChoiceQuestionTextRTB_ContentsResized() event handler – it sets the height of the
+    // "RichTextBox", which displays the text of the question / task formulation, from a lower value
+    // to a higher one, if the length of the text requires it.
+
+
+
         string _question;
         List<string> _answerOptions = new List<string>();
         public MultipleChoiceTaskUC()
