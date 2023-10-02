f212fc92d64718ed0551985f1d519ab8ea672c09
szszadam <szszadam@gmail.com>
2023-10-02 20:06:31 +0200

    Description of the Test Sheet Checker Selector User Control Class

--- LEAP-v0_3/Form-Classes/TestSheetCheckerSelectorUC.cs
+++ LEAP-v0_3/Form-Classes/TestSheetCheckerSelectorUC.cs
@@ -12,6 +12,37 @@ namespace LEAP_v0_3
 {
     public partial class TestSheetCheckerSelectorUC : UserControl
     {
+        //      ***** Test Sheet Checker Selector User Control Class *****
+        //
+        //
+        //      *** Class description ***
+        //
+        //
+        // This class is a descendant of the "UserControl" class, stands for selecting for checking/scoring
+        // individual test sheets completed and submitted by users.The main elements of its graphical
+        // interface are the following: the "DataGridView" control for selecting the submitted individual
+        // test sheet, the "Check selected test sheet" button, and the "Refresh table" button.
+        //
+        //
+        //      *** Methods and event handlers ***
+        //
+        //
+        // FillTestSheetSelectorDGV() –  It fills with data the "DataGridView" control listing individual
+        // test sheets.The method uses some auxiliary variables while it is running.
+        //
+        //
+        // TestSheetCheckerSelectorUC_Load() event handler – When the Test Sheet Checker Selector User
+        // Control is loaded, it calls the FillTestSheetSelectorDGV() method.
+        //
+        //
+        // SelectTestSheetButton_Click() event handler - This method runs when the "Check selected test
+        // sheet" button is clicked, which creates an instance of the " TestSheetCheckerWindow" class and
+        // opens it in a new window.
+        //
+        //
+        // RefreshTableButton_Click() event handler - This method runs when the "Update Table" button is
+        // clicked, which calls the FillTestSheetSelectorDGV() method.
+
         public TestSheetCheckerSelectorUC()
         {
             InitializeComponent();
